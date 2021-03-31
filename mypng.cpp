3fb5ff38b993dbecc58e041ac56fb94263f1978c
HBJ2020 <792947622@qq.com>
2021-03-31 09:32:00 +0800

    1.sign,IHDR, and IEND write tidied.

--- mypng.cpp
+++ mypng.cpp
@@ -255,9 +255,8 @@ static unsigned char paethPredictor(short a, short b, short c)
 static unsigned writeSignature(ucvector* out)
 {
   size_t pos = out->size;
-  const unsigned char signature[] = {137, 80, 78, 71, 13, 10, 26, 10};
-  /*8 bytes PNG signature, aka the magic bytes*/
-  if(!ucvector_resize(out, out->size + 8)) return 83; /*alloc fail*/
+  const unsigned char signature[] = {137, 80, 78, 71, 13, 10, 26, 10}; /*8 bytes PNG signature, aka the magic bytes*/
+  ucvector_resize(out, out->size + 8);
   lodepng_memcpy(out->data + pos, signature, 8);
   return 0;
 }
@@ -266,25 +265,34 @@ static unsigned addChunk_IHDR(ucvector* out, unsigned w, unsigned h, LodePNGColo
 {
   unsigned char *chunk, *data;
 
-  unsigned error = lodepng_chunk_init(&chunk, out, 13, "IHDR");
-  if(error) return error;
+  lodepng_chunk_init(&chunk, out, 13, "IHDR");
 
   data = chunk + 8;
-  lodepng_set32bitInt(data + 0, w); /*width*/
-  lodepng_set32bitInt(data + 4, h); /*height*/
-  data[8] = (unsigned char)bitdepth; /*bit depth*/
+  lodepng_set32bitInt(data + 0, w);   /*width*/
+  lodepng_set32bitInt(data + 4, h);   /*height*/
+  data[8] = (unsigned char)bitdepth;  /*bit depth*/
   data[9] = (unsigned char)colortype; /*color type*/
-  data[10] = 0; /*compression method*/
-  data[11] = 0; /*filter method*/
-  data[12] = interlace_method; /*interlace method*/
+  data[10] = 0;                       /*compression method*/
+  data[11] = 0;                       /*filter method*/
+  data[12] = interlace_method;        /*interlace method*/
 
   lodepng_chunk_generate_crc(chunk);
+
   return 0;
 }
 
 static unsigned addChunk_IEND(ucvector* out)
 {
-  return lodepng_chunk_createv(out, 0, "IEND", 0);
+  unsigned char* chunk;
+
+  lodepng_chunk_init(&chunk, out, 0, "IEND");
+
+  /*3: the data*/
+  lodepng_memcpy(chunk + 8, NULL, 0);
+  /*4: CRC (of the chunkname characters and the data)*/
+  lodepng_chunk_generate_crc(chunk);
+
+  return 0;
 }
 
 static unsigned addChunk_IDAT(ucvector* out, const unsigned char* data, size_t datasize, LodePNGCompressSettings* zlibsettings)
