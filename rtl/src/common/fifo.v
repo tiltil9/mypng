ebef865623f82336f671bb23740bf6524b0e906e
tiltil9 <20212020107@fudan.edu.cn>
2021-04-14 17:05:28 +0800

    common items (defines / ram / fifo) added

---
+++ rtl/src/common/fifo.v
@@ -0,0 +1,152 @@
+//******************************************************************************
+//
+//  Filename      : fifo.v
+//  Description   : register-based fifo
+//  Author        : Tingting Li
+//  Email         : 1107319548@qq.com
+//  Created       : 2021-04-14
+//
+//******************************************************************************
+
+`include "defines.vh"
+
+module fifo(
+    // global
+    clk         ,
+    rstn        ,
+    // write
+    wr_val_i    ,
+    wr_dat_i    ,
+    wr_ful_o    ,
+    // read
+    rd_val_i    ,
+    rd_val_o    ,
+    rd_dat_o    ,
+    rd_ept_o    ,
+    // common
+    wd_usd_o
+);
+
+
+//***   PARAMETER   ***********************************************************
+  // global
+  parameter     SIZE       = -1             ;
+  parameter     DATA_WD    = -1             ;
+
+  // derived
+  localparam    SIZE_WD    = `LOG2( SIZE )  ;
+
+
+//***   INPUT / OUTPUT   ******************************************************
+  // global
+  input                             clk       ;
+  input                             rstn      ;
+
+  // write
+  input                             wr_val_i  ;
+  input          [DATA_WD-1  :0]    wr_dat_i  ;
+  output                            wr_ful_o  ;
+  
+  // read
+  input                             rd_val_i  ;
+  output                            rd_val_o  ;
+  output        [DATA_WD-1   :0]    rd_dat_o  ;
+  output                            rd_ept_o  ;
+
+  // common
+  output reg    [SIZE_WD+1-1 :0]    wd_usd_o  ;
+
+
+//***   WIRE / REG   **********************************************************
+  reg           [SIZE_WD-1   :0]    wr_adr_r  ;
+  reg           [SIZE_WD-1   :0]    rd_adr_r  ;
+  wire          [SIZE_WD-1   :0]    adr_w     ;
+
+
+//***   MAIN BODY   ***********************************************************
+//---   WRITE   -----------------------------------------
+  // wr_ful_o
+  assign wr_ful_o = wd_usd_o == SIZE ;
+
+  // wr_adr_r
+  always @(posedge clk or negedge rstn ) begin
+    if( !rstn ) begin
+      wr_adr_r <= 'd0 ;
+    end
+    else if( wr_val_i ) begin
+      if( wr_adr_r == SIZE - 'd1 ) begin
+        wr_adr_r <= 'd0 ;
+      end
+      else begin
+        wr_adr_r <= wr_adr_r + 'd1 ;
+      end
+    end
+  end
+
+
+//---   READ   ------------------------------------------
+  // rd_ept_o
+  assign rd_ept_o = wd_usd_o == 'd0 ;
+
+  // rd_adr_r
+  always @(posedge clk or negedge rstn ) begin
+    if( !rstn ) begin
+      rd_adr_r <= 'd0 ;
+    end
+    else if(rd_val_i) begin
+      if( rd_adr_r == SIZE - 'd1 ) begin
+        rd_adr_r <= 'd0 ;
+      end
+      else begin
+        rd_adr_r <= rd_adr_r + 'd1 ;
+      end
+    end
+  end 
+
+
+//---   COMMON   ------------------------------------------
+  // adr_w
+  assign adr_w = wr_val_i ? wr_adr_r : 
+                 rd_val_i ? rd_adr_r :
+                            'd0      ;
+
+  // wd_usd_o
+  always @(posedge clk or negedge rstn ) begin
+    if( !rstn ) begin
+      wd_usd_o <= 'd0 ;
+    end
+    else if( wr_val_i ) begin
+      if( wd_usd_o == SIZE ) begin
+        wd_usd_o <= wd_usd_o ;
+      end
+      else begin
+        wd_usd_o <= wd_usd_o + 'd1 ;
+      end
+    end
+    else if( rd_val_i ) begin
+      if( wd_usd_o == 'd0 ) begin
+        wd_usd_o <= wd_usd_o ;
+      end
+      else begin
+        wd_usd_o <= wd_usd_o - 'd1 ;
+      end
+    end
+  end
+
+//--- MEMORY ARRAY ----------------------------------------
+  // ram
+  ram #(
+    .SIZE     ( SIZE    ),
+    .DATA_WD  ( DATA_WD )
+  ) ram(
+    .clk      ( clk      ),
+    .rstn     ( rstn     ),
+    .adr_i    ( adr_w    ),
+    .wr_val_i ( wr_val_i ),
+    .wr_dat_i ( wr_dat_i ),
+    .rd_val_i ( rd_val_i ),
+    .rd_val_o ( rd_val_o ),
+    .rd_dat_o ( rd_dat_o )
+  );
+
+endmodule
---
+++ rtl/src/common/ram.v
@@ -0,0 +1,91 @@
+//******************************************************************************
+//
+//  Filename      : ram.v
+//  Description   : register-based single-port ram
+//  Author        : Tingting Li
+//  Email         : 1107319548@qq.com
+//  Created       : 2021-04-14
+//
+//******************************************************************************
+
+`include "defines.vh"
+
+module ram(
+  // global
+  clk         ,
+  rstn        ,
+  // common
+  adr_i       ,
+  // write
+  wr_val_i    ,
+  wr_dat_i    ,
+  // read
+  rd_val_i    ,
+  rd_val_o    ,
+  rd_dat_o
+);
+
+
+//***   PARAMETER   ***********************************************************
+  // global
+  parameter     SIZE       = -1             ;
+  parameter     DATA_WD    = -1             ;
+
+  // derived
+  localparam    SIZE_WD    = `LOG2( SIZE )  ;
+
+
+//***   INPUT / OUTPUT   ******************************************************
+  // global
+  input                           clk       ;
+  input                           rstn      ;
+
+  // common
+  input         [SIZE_WD-1 :0]    adr_i     ;
+
+  // write
+  input                           wr_val_i  ;
+  input         [DATA_WD-1 :0]    wr_dat_i  ;
+  
+  // read
+  input                           rd_val_i  ;
+  output reg                      rd_val_o  ;
+  output reg    [DATA_WD-1 :0]    rd_dat_o  ;
+
+
+//***   WIRE / REG   **********************************************************
+  reg           [DATA_WD-1 :0]    mem_array[SIZE-1 :0]  ;
+
+
+//***   MAIN BODY   ***********************************************************
+//---   WRITE   -----------------------------------------
+  // mem_array
+  always @(posedge clk) begin
+    if( wr_val_i ) begin
+      mem_array[adr_i] <= wr_dat_i ;
+    end  
+  end
+
+
+//---   READ   ------------------------------------------
+  // rd_val_o
+  always @(posedge clk or negedge rstn) begin
+    if( !rstn ) begin
+      rd_val_o <= 'd0 ;
+    end
+    else begin
+      rd_val_o <= rd_val_i ;
+    end
+  end
+
+  // rd_dat_o
+  always @(posedge clk or negedge rstn ) begin
+    if( !rstn ) begin
+      rd_dat_o <= 'd0 ;
+    end
+    else if( rd_val_i ) begin
+      rd_dat_o <= mem_array[adr_i] ;
+    end
+  end
+
+endmodule
