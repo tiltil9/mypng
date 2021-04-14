f123e4899ef2914d74b8cd43fabe3dca59f9d5f8
HBJ2020 <792947622@qq.com>
2021-04-14 23:30:57 +0800

    1.crc32_tb.v reorganzied.

--- rtl/src/crc32/crc32_tb.v
+++ rtl/src/crc32/crc32_tb.v
@@ -1,15 +1,36 @@
-module tb;
+/******************************************************************************
+*******************************************************************************
+**                                                                           **
+**  File        :
+**  Contributor :
+**  Date        :
+**  Description :
+**                                                                           **
+*******************************************************************************
+******************************************************************************/
 
-reg                   clk    ;
-reg                   rstn   ;
-reg                   start_i;
-reg                   val_i  ;
-reg  [8       -1 :0]  dat_i  ; // temp
-reg                   lst_i  ;
-wire                  done_o ;
-wire                  val_o  ;
-wire [32      -1 :0]  dat_o  ;
+//--- LOCAL (CONSTANT) -----------------
+`define CLK_FULL    10
+`define CLK_HALF    (`CLK_FULL / 2)
 
+module crc2_tb;
+
+//*** PARAMETER ****************************************************************
+  localparam DATA_WD  = 'd32;
+
+//*** INPUT/OUTPUT *************************************************************
+  reg                   clk    ;
+  reg                   rstn   ;
+  reg                   start_i;
+  reg                   val_i  ;
+  reg  [8       -1 :0]  dat_i  ; // temp
+  reg                   lst_i  ;
+  wire                  done_o ;
+  wire                  val_o  ;
+  wire [DATA_WD -1 :0]  dat_o  ;
+
+//*** MAIN BODY ****************************************************************
+//--- INST -----------------------------
 crc32 dut(.clk    (clk    ),
           .rstn   (rstn   ),
           .start_i(start_i),
@@ -20,36 +41,43 @@ crc32 dut(.clk    (clk    ),
           .val_o  (val_o  ),
           .dat_o  (dat_o  ) );
 
-initial begin
-    clk = 1'b0;
-    forever 
-	begin
-		#10 clk = ~clk;
-	end
-end
+//--- PROC -----------------------------
+  // clk
+  initial begin
+    clk = 1'b0 ;
+    forever begin
+      #`CLK_HALF;
+      clk = ~clk;
+    end
+  end
 
-initial
-begin
-    val_i = 1'b0;
+  // rstn
+  initial begin
     rstn = 1'b0;
-	repeat(1) @(posedge clk);
-	rstn = 1'b1;
+    #(5 * `CLK_FULL);
+    @(negedge clk);
+    rstn = 'd1 ;
+  end
+
+  // main
+  initial begin
+    val_i = 1'b0;
+    #(5 * `CLK_FULL); // !!! this delay is essential.
     #1;
-	repeat(1) @(posedge clk);
+    repeat(1) @(posedge clk);
     dat_i = 8'h4;
     val_i = 1'b1;
     #1 ;
-	repeat(1) @(posedge clk);
+    repeat(1) @(posedge clk);
     val_i = 1'b0; // d56f2b94
     #1;
-	repeat(1) @(posedge clk);
+    repeat(1) @(posedge clk);
     dat_i = 8'h9;
     val_i = 1'b1;
     #1 ;
-	repeat(1) @(posedge clk);
+    repeat(1) @(posedge clk);
     val_i = 1'b0; // 5c696f5f
-
-end
+  end
 
 
 endmodule
