932184dcf9888e2f2702f494be5b75402927c66c
HBJ2020 <792947622@qq.com>
2021-04-25 21:00:32 +0800

    1.bsCat codes and tb simply added.

--- rtl/src/bsCoding/bsCat.v
+++ rtl/src/bsCoding/bsCat.v
@@ -24,21 +24,65 @@ module bsCat(
   localparam DATA_WD         = 'd32;
   localparam NUMB_WD         = 'd5 ;
 
+  localparam PTR_OUT_BUF_WD  = 'd5 ; /*`LOG2(DATA_WD)*/
+
 //***   INPUT / OUTPUT   ******************************************************
   //
-  input                      clk   ;
-  input                      rstn  ;
+  input                             clk          ;
+  input                             rstn         ;
   //
-  input                      val_i ;
-  input      [DATA_WD -1 :0] dat_i ;
-  input      [NUMB_WD -1 :0] numb_i; // means 1~32 least significant bits are valid
+  input                             val_i        ;
+  input      [DATA_WD        -1 :0] dat_i        ;
+  input      [NUMB_WD        -1 :0] numb_i       ; // means 1~32 least significant bits are valid
   //
-  output reg                 val_o ;
-  output     [DATA_WD -1 :0] dat_o ;
+  output reg                        val_o        ;
+  output     [DATA_WD        -1 :0] dat_o        ;
 
 //***   WIRE / REG   **********************************************************
+  // output buffer
+  reg        [DATA_WD*2      -1 :0] dat_out_buf_r;
+  reg        [PTR_OUT_BUF_WD -1 :0] ptr_out_buf_r;
 
 //***   MAIN BODY   ***********************************************************
+//---   CAT   ---------------------------------------------
+  // data output buffer
+  always @(posedge clk or negedge rstn) begin
+    if (!rstn) begin
+      dat_out_buf_r <= 'd0;
+    end
+    else if (val_i) begin
+      dat_out_buf_r <= (dat_out_buf_r << (numb_i + 'd1)) | dat_i; // precede 0 of dat_i does not matter
+    end
+  end
+
+  // pointer output buffer
+  always @(posedge clk or negedge rstn) begin
+    if (!rstn) begin
+      ptr_out_buf_r <= 'd0;
+    end
+    else if (val_i) begin
+      ptr_out_buf_r <= (ptr_out_buf_r + (numb_i + 'd1) >= DATA_WD) ? (ptr_out_buf_r + (numb_i + 'd1) - DATA_WD) : (ptr_out_buf_r + (numb_i + 'd1));
+    end
+  end
+
+//---   OUTPUT   ------------------------------------------
+  // val_o
+  always @(posedge clk or negedge rstn) begin
+    if (!rstn) begin
+      val_o <= 'd0;
+    end
+    else begin
+      if (val_i && ptr_out_buf_r + (numb_i + 'd1) >= DATA_WD) begin
+        val_o <= 'd1;
+      end
+      else begin
+        val_o <= 'd0;
+      end
+    end
+  end
+
+  // dat_o
+  assign dat_o = (dat_out_buf_r >> ptr_out_buf_r); // [DATA_WD -1 :0] // TODO: reverse per byte
 
 
 endmodule
--- rtl/src/bsCoding/bsCat_tb.v
+++ rtl/src/bsCoding/bsCat_tb.v
@@ -7,3 +7,108 @@
   //  Created       : xxxx-xx-xx
   //
 //------------------------------------------------------------------------------
+
+//---   LOCAL (CONSTANT)   --------------------------------
+`define CLK_FULL    10
+`define CLK_HALF    (`CLK_FULL / 2)
+
+module bsCat_tb;
+
+//***   PARAMETER   ***********************************************************
+  localparam DATA_WD  = 'd32;
+  localparam NUMB_WD  = 'd5 ;
+
+//***   INPUT / OUTPUT   ******************************************************
+  reg                   clk    ;
+  reg                   rstn   ;
+  reg                   val_i  ;
+  reg  [DATA_WD -1 :0]  dat_i  ;
+  reg  [NUMB_WD -1 :0]  numb_i ;
+  wire                  val_o  ;
+  wire [DATA_WD -1 :0]  dat_o  ;
+
+//***   MAIN BODY   ***********************************************************
+//---   INST   --------------------------------------------
+bsCat dut(.clk    (clk    ),
+          .rstn   (rstn   ),
+          .val_i  (val_i  ),
+          .dat_i  (dat_i  ),
+          .numb_i (numb_i ),
+          .val_o  (val_o  ),
+          .dat_o  (dat_o  ) );
+
+//---   PROC   --------------------------------------------
+  // clk
+  initial begin
+    clk = 1'b0 ;
+    forever begin
+      #`CLK_HALF;
+      clk = ~clk;
+    end
+  end
+
+  // rstn
+  initial begin
+    rstn = 1'b0;
+    #(5 * `CLK_FULL);
+    @(negedge clk);
+    rstn = 'd1 ;
+  end
+
+  // main
+  initial begin
+    val_i = 1'b0;
+    numb_i = 'd0;
+    #(5 * `CLK_FULL);
+    @(negedge clk); // !!! this delay is essential.
+
+    @(posedge clk);
+    @(negedge clk);
+    @(posedge clk);
+    dat_i = 32'h00000409;
+    val_i = 1'b1;
+    numb_i = 'd15;
+    @(negedge clk);
+    @(posedge clk);
+    val_i = 1'b0; 
+    dat_i = 32'h0;
+    numb_i = 'd0;
+
+    @(negedge clk);
+    @(posedge clk);
+    dat_i = 32'h09040409;
+    val_i = 1'b1;
+    numb_i = 'd31;
+    @(negedge clk);
+    @(posedge clk);
+    val_i = 1'b0; 
+    dat_i = 32'h0;
+    numb_i = 'd0;
+
+    @(negedge clk);
+    @(posedge clk);
+    dat_i = 32'h0000000f;
+    val_i = 1'b1;
+    numb_i = 'd3;
+    @(negedge clk);
+    @(posedge clk);
+    val_i = 1'b0; 
+    dat_i = 32'h0;
+    numb_i = 'd0;
+
+    @(posedge clk);
+    @(negedge clk);
+    @(posedge clk);
+    dat_i = 32'h0000abcd;
+    val_i = 1'b1;
+    numb_i = 'd15;
+    @(negedge clk);
+    @(posedge clk);
+    val_i = 1'b0; 
+    dat_i = 32'h0;
+    numb_i = 'd0;
+
+  end
+
+
+endmodule
