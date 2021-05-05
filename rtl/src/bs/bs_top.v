b2ad29b74b75c0b03c9fc1ab4c710618d5f27fae
HBJ2020 <792947622@qq.com>
2021-05-05 21:02:35 +0800

    1.byte length of zlib data added in bs output.

--- rtl/src/bs/bs_top.v
+++ rtl/src/bs/bs_top.v
@@ -37,15 +37,16 @@ module bs_top(
   localparam DATA_WD     = 'd32;
 
   // fsm
-  localparam FSM_WD      =  'd3;
-  localparam IDLE        = 3'd0;
-  localparam CMF_FLG     = 3'd1; // CMF, FLG
-  localparam BLK_0       = 3'd2; // BFINAL, BTYPE
-  localparam BLK_1       = 3'd3; // compressed data block
-  localparam BLK_2       = 3'd4; // end of block
-  localparam BLK_3       = 3'd5; // flush 0 to align with byte boundary
-  localparam ADLER32     = 3'd6; // adler32
-  localparam FLUSH       = 3'd7; // flush 0 to output remain // TODO: may add num_o
+  localparam FSM_WD      =  'd4;
+  localparam IDLE        = 4'd0;
+  localparam CMF_FLG     = 4'd1; // CMF, FLG
+  localparam BLK_0       = 4'd2; // BFINAL, BTYPE
+  localparam BLK_1       = 4'd3; // compressed data block
+  localparam BLK_2       = 4'd4; // end of block
+  localparam BLK_3       = 4'd5; // flush 0 to align with byte boundary
+  localparam ADLER32     = 4'd6; // adler32
+  localparam FLUSH       = 4'd7; // flush 0 to output remain // TODO: may add num_o
+  localparam ZLIB_LEN    = 4'd8; // byte length of zlib data, assume no more than (1 << 32 - 1)
 
   // huffman fixed
   localparam HUF_CODE_WD = 'd19;
@@ -54,6 +55,9 @@ module bs_top(
   // delay count for waiting crc32 process
   localparam DLY_CNT     = 'd4 ;
   localparam DLY_CNT_WD  = 'd2 ; /*`LOG2(DLY_CNT)*/
+
+  // byte length of zlib data
+  localparam ZLIB_LEN_WD = 'd32;
 
   // bitstream output
   localparam NUMB_WD     = 'd5 ;
@@ -93,6 +97,10 @@ module bs_top(
   // adler32 buffer
   reg    [DATA_WD     -1 :0] adler32_dat_buf_r  ;
 
+  // byte length of zlib data
+  wire   [ZLIB_LEN_WD -1 :0] zlib_len_nxt_w     ;
+  reg    [ZLIB_LEN_WD -1 :0] zlib_len_r         ;
+
   // flush 0 numb accumulator
   wire   [NUMB_WD     -1 :0] bs_flush_numb_nxt_w;
   reg    [NUMB_WD     -1 :0] bs_flush_numb_r    ;
@@ -120,21 +128,22 @@ module bs_top(
   always @(*) begin
     nxt_state_w = IDLE;
     case (cur_state_r)
-      IDLE   : if (start_i)                         nxt_state_w = CMF_FLG;
-               else                                 nxt_state_w = IDLE   ;
-      CMF_FLG:                                      nxt_state_w = BLK_0  ;
-      BLK_0  :                                      nxt_state_w = BLK_1  ;
-      BLK_1  : if (lst_i)                           nxt_state_w = BLK_2  ;
-               else                                 nxt_state_w = BLK_1  ;
-      BLK_2  : if (dly_cnt_r == DLY_CNT - 'd1)      nxt_state_w = bs_flush_numb_nxt_w[2:0] != 'd0 ? BLK_3 : ADLER32;
-               else                                 nxt_state_w = BLK_2  ;
-      BLK_3  : if (dly_cnt_r == DLY_CNT - 'd1)      nxt_state_w = ADLER32;
-               else                                 nxt_state_w = BLK_3  ;
-      ADLER32: if (dly_cnt_r == DLY_CNT - 'd1)      nxt_state_w = bs_flush_numb_nxt_w != 'd0 ? FLUSH : IDLE;
-               else                                 nxt_state_w = ADLER32;
-      FLUSH  : if (dly_cnt_r == DLY_CNT - 'd1)      nxt_state_w = IDLE   ;
-               else                                 nxt_state_w = FLUSH  ;
-      default:                                      nxt_state_w = IDLE   ;
+      IDLE    : if (start_i)                    nxt_state_w = CMF_FLG ;
+                else                            nxt_state_w = IDLE    ;
+      CMF_FLG :                                 nxt_state_w = BLK_0   ;
+      BLK_0   :                                 nxt_state_w = BLK_1   ;
+      BLK_1   : if (lst_i)                      nxt_state_w = BLK_2   ;
+                else                            nxt_state_w = BLK_1   ;
+      BLK_2   : if (dly_cnt_r == DLY_CNT - 'd1) nxt_state_w = bs_flush_numb_nxt_w[2:0] != 'd0 ? BLK_3 : ADLER32;
+                else                            nxt_state_w = BLK_2   ;
+      BLK_3   : if (dly_cnt_r == DLY_CNT - 'd1) nxt_state_w = ADLER32 ;
+                else                            nxt_state_w = BLK_3   ;
+      ADLER32 : if (dly_cnt_r == DLY_CNT - 'd1) nxt_state_w = bs_flush_numb_nxt_w != 'd0 ? FLUSH : ZLIB_LEN;
+                else                            nxt_state_w = ADLER32 ;
+      FLUSH   : if (dly_cnt_r == DLY_CNT - 'd1) nxt_state_w = ZLIB_LEN;
+                else                            nxt_state_w = FLUSH   ;
+      ZLIB_LEN:                                 nxt_state_w = IDLE    ;
+      default :                                 nxt_state_w = IDLE    ;
     endcase
   end
 
@@ -173,6 +182,21 @@ module bs_top(
     end
   end
 
+  // byte length of zlib data
+  assign zlib_len_nxt_w = zlib_len_r + ((bs_flush_numb_r[2:0] + bs_out_numb_i_w + 'd1) >> 'd3);
+
+  always @(posedge clk or negedge rstn) begin
+    if (!rstn) begin
+      zlib_len_r <= 'd0;
+    end
+    else if (cur_state_r == CMF_FLG || cur_state_r == BLK_0 || cur_state_r == BLK_1
+           ||cur_state_r == BLK_2 || cur_state_r == BLK_3 || cur_state_r == ADLER32) begin
+      if (bs_out_val_i_w) begin
+        zlib_len_r <= zlib_len_nxt_w;
+      end
+    end
+  end
+
 //---   BITSTREAM OUTPUT  ---------------------------------
   // flush 0 numb accumulator
   assign bs_flush_numb_nxt_w = bs_flush_numb_r + (bs_out_numb_i_w + 'd1); // mod 32 acc
@@ -190,40 +214,49 @@ module bs_top(
   assign bs_out_val_i_w = (cur_state_r == CMF_FLG || cur_state_r == BLK_0)
                        || (cur_state_r == BLK_1 && val_i)  // !!! LZ77 data should only appear in this period
                        || ((cur_state_r == BLK_2 || cur_state_r == BLK_3 || cur_state_r == ADLER32 || cur_state_r == FLUSH) && (dly_cnt_r == DLY_CNT - 'd1))  // all need to wait crc32 process
-                       ;
+                       || (cur_state_r == ZLIB_LEN);
 
   always @(*) begin
     bs_out_dat_i_w = 'd0;
     case (cur_state_r)
-      CMF_FLG: bs_out_dat_i_w = 'b00011110_10000000; // !!! all bs_out_dat_i_w should reverse per byte
-      BLK_0  : bs_out_dat_i_w = 'b1_10;
-      BLK_1  : bs_out_dat_i_w = huf_code_w;
-      BLK_2  : bs_out_dat_i_w = 'b0000000; // end of block huffman fixed code
-      BLK_3  : bs_out_dat_i_w = 'b0;
-      ADLER32: bs_out_dat_i_w = {adler32_dat_buf_r[24], adler32_dat_buf_r[25], adler32_dat_buf_r[26], adler32_dat_buf_r[27],
-                                 adler32_dat_buf_r[28], adler32_dat_buf_r[29], adler32_dat_buf_r[30], adler32_dat_buf_r[31],
-                                 adler32_dat_buf_r[16], adler32_dat_buf_r[17], adler32_dat_buf_r[18], adler32_dat_buf_r[19],
-                                 adler32_dat_buf_r[20], adler32_dat_buf_r[21], adler32_dat_buf_r[22], adler32_dat_buf_r[23],
-                                 adler32_dat_buf_r[ 8], adler32_dat_buf_r[ 9], adler32_dat_buf_r[10], adler32_dat_buf_r[11],
-                                 adler32_dat_buf_r[12], adler32_dat_buf_r[13], adler32_dat_buf_r[14], adler32_dat_buf_r[15],
-                                 adler32_dat_buf_r[ 0], adler32_dat_buf_r[ 1], adler32_dat_buf_r[ 2], adler32_dat_buf_r[ 3],
-                                 adler32_dat_buf_r[ 4], adler32_dat_buf_r[ 5], adler32_dat_buf_r[ 6], adler32_dat_buf_r[ 7]};
-      FLUSH  : bs_out_dat_i_w = 'b0;
-      default: bs_out_dat_i_w = 'b0;
+      CMF_FLG : bs_out_dat_i_w = 'b00011110_10000000; // !!! all bs_out_dat_i_w should reverse per byte
+      BLK_0   : bs_out_dat_i_w = 'b1_10;
+      BLK_1   : bs_out_dat_i_w = huf_code_w;
+      BLK_2   : bs_out_dat_i_w = 'b0000000; // end of block huffman fixed code
+      BLK_3   : bs_out_dat_i_w = 'b0;
+      ADLER32 : bs_out_dat_i_w = {adler32_dat_buf_r[24], adler32_dat_buf_r[25], adler32_dat_buf_r[26], adler32_dat_buf_r[27],
+                                  adler32_dat_buf_r[28], adler32_dat_buf_r[29], adler32_dat_buf_r[30], adler32_dat_buf_r[31],
+                                  adler32_dat_buf_r[16], adler32_dat_buf_r[17], adler32_dat_buf_r[18], adler32_dat_buf_r[19],
+                                  adler32_dat_buf_r[20], adler32_dat_buf_r[21], adler32_dat_buf_r[22], adler32_dat_buf_r[23],
+                                  adler32_dat_buf_r[ 8], adler32_dat_buf_r[ 9], adler32_dat_buf_r[10], adler32_dat_buf_r[11],
+                                  adler32_dat_buf_r[12], adler32_dat_buf_r[13], adler32_dat_buf_r[14], adler32_dat_buf_r[15],
+                                  adler32_dat_buf_r[ 0], adler32_dat_buf_r[ 1], adler32_dat_buf_r[ 2], adler32_dat_buf_r[ 3],
+                                  adler32_dat_buf_r[ 4], adler32_dat_buf_r[ 5], adler32_dat_buf_r[ 6], adler32_dat_buf_r[ 7]};
+      FLUSH   : bs_out_dat_i_w = 'b0;
+      ZLIB_LEN: bs_out_dat_i_w = {zlib_len_r[24], zlib_len_r[25], zlib_len_r[26], zlib_len_r[27],
+                                  zlib_len_r[28], zlib_len_r[29], zlib_len_r[30], zlib_len_r[31],
+                                  zlib_len_r[16], zlib_len_r[17], zlib_len_r[18], zlib_len_r[19],
+                                  zlib_len_r[20], zlib_len_r[21], zlib_len_r[22], zlib_len_r[23],
+                                  zlib_len_r[ 8], zlib_len_r[ 9], zlib_len_r[10], zlib_len_r[11],
+                                  zlib_len_r[12], zlib_len_r[13], zlib_len_r[14], zlib_len_r[15],
+                                  zlib_len_r[ 0], zlib_len_r[ 1], zlib_len_r[ 2], zlib_len_r[ 3],
+                                  zlib_len_r[ 4], zlib_len_r[ 5], zlib_len_r[ 6], zlib_len_r[ 7]};
+      default : bs_out_dat_i_w = 'b0;
     endcase
   end
 
   always @(*) begin
     bs_out_numb_i_w = 'd0;
     case (cur_state_r)
-      CMF_FLG: bs_out_numb_i_w = 'd16 - 'd1;
-      BLK_0  : bs_out_numb_i_w = 'd3 - 'd1;
-      BLK_1  : bs_out_numb_i_w = huf_code_w_d_w - 'd1;
-      BLK_2  : bs_out_numb_i_w = 'd7 - 'd1;
-      BLK_3  : bs_out_numb_i_w = ('d8 - bs_flush_numb_r[2:0] -'d1);
-      ADLER32: bs_out_numb_i_w = 'd32 - 'd1;
-      FLUSH  : bs_out_numb_i_w = ('d32 - bs_flush_numb_r - 'd1);
-      default: bs_out_numb_i_w = 'd0;
+      CMF_FLG : bs_out_numb_i_w = 'd16 - 'd1;
+      BLK_0   : bs_out_numb_i_w = 'd3 - 'd1;
+      BLK_1   : bs_out_numb_i_w = huf_code_w_d_w - 'd1;
+      BLK_2   : bs_out_numb_i_w = 'd7 - 'd1;
+      BLK_3   : bs_out_numb_i_w = ('d8 - bs_flush_numb_r[2:0] -'d1);
+      ADLER32 : bs_out_numb_i_w = 'd32 - 'd1;
+      FLUSH   : bs_out_numb_i_w = ('d32 - bs_flush_numb_r - 'd1);
+      ZLIB_LEN: bs_out_numb_i_w = 'd32 - 'd1;
+      default : bs_out_numb_i_w = 'd0;
     endcase
   end
 
