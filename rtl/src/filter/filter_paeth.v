c66de5a9b863ee919414324669f530f582d0d448
tiltil9 <20212020107@fudan.edu.cn>
2021-05-13 15:34:43 +0800

    critical path shortened

--- rtl/src/filter/filter_paeth.v
+++ rtl/src/filter/filter_paeth.v
@@ -29,9 +29,13 @@ module filter_paeth(
 
 
 //***   WIRE / REG   **********************************************************
-  wire    [DATA_WD+2-1 :0]      pa_w  ;
-  wire    [DATA_WD+2-1 :0]      pb_w  ;
+  wire    [DATA_WD+1-1 :0]      pa_w  ;
+  wire    [DATA_WD+1-1 :0]      pb_w  ;
   wire    [DATA_WD+2-1 :0]      pc_w  ;
+
+  wire signed [DATA_WD+1-1 :0]  pa_signed_w ;
+  wire signed [DATA_WD+1-1 :0]  pb_signed_w ;
+  wire signed [DATA_WD+2-1 :0]  pc_signed_w ;
 
 
 //***   MAIN BODY   ***********************************************************
@@ -42,12 +46,14 @@ module filter_paeth(
   //     pc = abs(p - c) = abs(a + b - 2c)
   //     if min{pa, pb, pc} = px, o = x
 
+  assign pa_signed_w = $signed(dat_b_i           -  dat_c_i     ) ;
+  assign pb_signed_w = $signed(dat_a_i           -  dat_c_i     ) ;
+  assign pc_signed_w = $signed(dat_a_i + dat_b_i - (dat_c_i << 1)) ;
+
   // px_w
-  assign pa_w = dat_b_i > dat_c_i ? (dat_b_i - dat_c_i) : (dat_c_i - dat_b_i) ;
-  assign pb_w = dat_a_i > dat_c_i ? (dat_a_i - dat_c_i) : (dat_c_i - dat_a_i) ;
-  assign pc_w = (dat_a_i + dat_b_i > 2*dat_c_i) ? 
-                (dat_a_i + dat_b_i - 2*dat_c_i) : 
-                (2*dat_c_i - dat_a_i - dat_b_i) ;
+  assign pa_w = pa_signed_w[DATA_WD+1-1] ? -pa_signed_w : pa_signed_w ;
+  assign pb_w = pb_signed_w[DATA_WD+1-1] ? -pb_signed_w : pb_signed_w ;
+  assign pc_w = pc_signed_w[DATA_WD+2-1] ? -pc_signed_w : pc_signed_w ;
 
   // dat_o
   assign dat_o = (pa_w <= pb_w && pa_w <= pc_w) ? dat_a_i :
