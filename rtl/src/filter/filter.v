5f2b696cc7ba8d4ceea231076b672100c30bf110
tiltil9 <20212020107@fudan.edu.cn>
2021-05-14 14:37:48 +0800

    sum_abs_4_r added for criticial path

--- rtl/src/filter/filter.v
+++ rtl/src/filter/filter.v
@@ -102,10 +102,12 @@ module filter(
   reg                                            flg_busy_opt_r   ;
   reg                                            flg_busy_opt_d1_r;
   reg                                            flg_busy_opt_d2_r;
+  reg                                            flg_busy_opt_d3_r;
   wire                                           flg_busy_fnl_w   ;
   reg                                            flg_busy_fnl_r   ;
   reg                                            flg_busy_fnl_d1_r;
   reg                                            flg_busy_fnl_d2_r;
+  reg                                            flg_busy_fnl_d3_r;
   wire                                           flg_busy_w       ;
   reg                                            flg_busy_r       ;
   reg                                            flg_busy_d1_r    ;
@@ -119,10 +121,12 @@ module filter(
   reg           [`SIZE_W_WD-1             :0]    cnt_w_d1_r       ;
   reg           [`SIZE_W_WD-1             :0]    cnt_w_d2_r       ;
   reg           [`SIZE_W_WD-1             :0]    cnt_w_d3_r       ;
+  reg           [`SIZE_W_WD-1             :0]    cnt_w_d4_r       ;
   wire                                           cnt_w_done_w     ;
   reg                                            cnt_w_done_r     ;
   reg                                            cnt_w_done_d1_r  ;
   reg                                            cnt_w_done_d2_r  ;
+  reg                                            cnt_w_done_d3_r  ;
   reg           [`SIZE_H_WD-1             :0]    cnt_h_r          ;
   wire                                           cnt_h_done_w     ;
   reg           [DATA_CYC_WD-1            :0]    cnt_cmp_r        ;  // cmp -> compare
@@ -172,6 +176,17 @@ module filter(
   reg           [`DATA_CHN_WD+4+`SIZE_W_WD-1:0]  sum_m_w     ; 
   wire          [`DATA_CHN_WD+4+`SIZE_W_WD-1:0]  sum_n_w     ; 
   wire signed   [`DATA_CHN_WD+4+`SIZE_W_WD  :0]  sum_dlt_w   ;
+  wire          [`DATA_CHN_WD+1-1           :0]  sum_abs_0_w ;
+  wire          [`DATA_CHN_WD+1-1           :0]  sum_abs_1_w ;
+  wire          [`DATA_CHN_WD+1-1           :0]  sum_abs_2_w ;
+  wire          [`DATA_CHN_WD+1-1           :0]  sum_abs_3_w ;
+  wire          [`DATA_CHN_WD+1-1           :0]  sum_abs_4_w ;
+  reg           [`DATA_CHN_WD+1-1           :0]  sum_abs_0_r ;
+  reg           [`DATA_CHN_WD+1-1           :0]  sum_abs_1_r ;
+  reg           [`DATA_CHN_WD+1-1           :0]  sum_abs_2_r ;
+  reg           [`DATA_CHN_WD+1-1           :0]  sum_abs_3_r ;
+  reg           [`DATA_CHN_WD+1-1           :0]  sum_abs_4_r ;
+  
   
   // filter types
   reg           [`FILTER_ENUM_WD-1        :0]    typ_bst_r   ;
@@ -210,8 +225,8 @@ module filter(
 
   // jump condition
   assign cnt_w_done_w    = cnt_w_r    == (cfg_w_i - 'd1)  ;
-  assign opt_done_w      = flg_opt_w && cnt_w_done_d2_r   ;
-  assign fnl_done_w      = flg_fnl_w && cnt_w_done_d2_r   ;
+  assign opt_done_w      = flg_opt_w && cnt_w_done_d3_r   ;
+  assign fnl_done_w      = flg_fnl_w && cnt_w_done_d3_r   ;
 
   assign cnt_h_done_w    = cnt_h_r    == (cfg_h_i  - 'd1) ;
 
@@ -222,7 +237,8 @@ module filter(
   assign flg_busy_opt_w = flg_opt_w      &&  val_i           ;
   assign flg_busy_fnl_w = flg_fnl_w      && !cnt_w_done_r 
                                          && !cnt_w_done_d1_r 
-                                         && !cnt_w_done_d2_r ;
+                                         && !cnt_w_done_d2_r
+                                         && !cnt_w_done_d3_r ;
   assign flg_busy_w     = flg_busy_opt_w ||  flg_busy_fnl_w  ;
 
   // fetch previous and current scanline data
@@ -250,7 +266,7 @@ module filter(
       cnt_h_r <= 'd0 ;
     end
     else begin
-      if( cnt_w_done_d2_r && flg_fnl_w) begin
+      if( cnt_w_done_d3_r && flg_fnl_w) begin
         if( cnt_h_done_w ) cnt_h_r <= 'd0           ;
         else               cnt_h_r <= cnt_h_r + 'd1 ;
       end
@@ -278,15 +294,19 @@ module filter(
       cnt_w_d1_r        <= 'd0 ;
       cnt_w_d2_r        <= 'd0 ;
       cnt_w_d3_r        <= 'd0 ;
+      cnt_w_d4_r        <= 'd0 ;
       cnt_w_done_r      <= 'd0 ;
       cnt_w_done_d1_r   <= 'd0 ;
       cnt_w_done_d2_r   <= 'd0 ;
+      cnt_w_done_d3_r   <= 'd0 ;
       flg_busy_opt_r    <= 'd0 ;
       flg_busy_opt_d1_r <= 'd0 ;
       flg_busy_opt_d2_r <= 'd0 ;
+      flg_busy_opt_d3_r <= 'd0 ;
       flg_busy_fnl_r    <= 'd0 ;
       flg_busy_fnl_d1_r <= 'd0 ;
       flg_busy_fnl_d2_r <= 'd0 ;
+      flg_busy_fnl_d3_r <= 'd0 ;
       flg_busy_r        <= 'd0 ;
       flg_busy_d1_r     <= 'd0 ;
       dat_r             <= 'd0 ;
@@ -300,20 +320,29 @@ module filter(
       fifo_cur_wr_dat_o <= 'd0 ;
       fifo_flt_wr_val_o <= 'd0 ;
       fifo_flt_wr_dat_o <= 'd0 ;
+      sum_abs_0_r       <= 'd0 ;
+      sum_abs_1_r       <= 'd0 ;
+      sum_abs_2_r       <= 'd0 ;
+      sum_abs_3_r       <= 'd0 ;
+      sum_abs_4_r       <= 'd0 ;
     end
     else begin
       cnt_w_d1_r        <= cnt_w_r           ;
       cnt_w_d2_r        <= cnt_w_d1_r        ;
       cnt_w_d3_r        <= cnt_w_d2_r        ;
+      cnt_w_d4_r        <= cnt_w_d3_r        ;
       cnt_w_done_r      <= cnt_w_done_w      ;
       cnt_w_done_d1_r   <= cnt_w_done_r      ;
       cnt_w_done_d2_r   <= cnt_w_done_d1_r   ;
+      cnt_w_done_d3_r   <= cnt_w_done_d2_r   ;
       flg_busy_opt_r    <= flg_busy_opt_w    ;
       flg_busy_opt_d1_r <= flg_busy_opt_r    ;
       flg_busy_opt_d2_r <= flg_busy_opt_d1_r ;
+      flg_busy_opt_d3_r <= flg_busy_opt_d2_r ;
       flg_busy_fnl_r    <= flg_busy_fnl_w    ;
       flg_busy_fnl_d1_r <= flg_busy_fnl_r    ;
       flg_busy_fnl_d2_r <= flg_busy_fnl_d1_r ;
+      flg_busy_fnl_d3_r <= flg_busy_fnl_d2_r ;
       flg_busy_r        <= flg_busy_w        ;
       flg_busy_d1_r     <= flg_busy_r        ;
       dat_r             <= dat_i             ;
@@ -327,6 +356,11 @@ module filter(
       fifo_cur_wr_dat_o <= dat_i             ;
       fifo_flt_wr_val_o <= fifo_flt_wr_val_w ;
       fifo_flt_wr_dat_o <= fifo_flt_wr_dat_w ;
+      sum_abs_0_r       <= sum_abs_0_w       ;
+      sum_abs_1_r       <= sum_abs_1_w       ;
+      sum_abs_2_r       <= sum_abs_2_w       ;
+      sum_abs_3_r       <= sum_abs_3_w       ;
+      sum_abs_4_r       <= sum_abs_4_w       ;
     end
   end
 
@@ -408,11 +442,11 @@ module filter(
 
 // ---   FSM: OPT   ---------------------------------------
   // sum_x_w
-  assign sum_0_w = (cnt_w_d3_r == 'd0) ? 'd0 : sum_0_r;
-  assign sum_1_w = (cnt_w_d3_r == 'd0) ? 'd0 : sum_1_r;
-  assign sum_2_w = (cnt_w_d3_r == 'd0) ? 'd0 : sum_2_r;
-  assign sum_3_w = (cnt_w_d3_r == 'd0) ? 'd0 : sum_3_r;
-  assign sum_4_w = (cnt_w_d3_r == 'd0) ? 'd0 : sum_4_r;
+  assign sum_0_w = (cnt_w_d4_r == 'd0) ? 'd0 : sum_0_r;
+  assign sum_1_w = (cnt_w_d4_r == 'd0) ? 'd0 : sum_1_r;
+  assign sum_2_w = (cnt_w_d4_r == 'd0) ? 'd0 : sum_2_r;
+  assign sum_3_w = (cnt_w_d4_r == 'd0) ? 'd0 : sum_3_r;
+  assign sum_4_w = (cnt_w_d4_r == 'd0) ? 'd0 : sum_4_r;
 
   // sum_x_r
   always @(posedge clk or negedge rstn ) begin
@@ -424,16 +458,21 @@ module filter(
       sum_4_r <= 'd0 ;
     end
     else begin
-      if( flg_busy_opt_d2_r ) begin
-        sum_0_r <= sum_0_w + res_abs_0_w[0 +:8] + res_abs_0_w[8 +:8] + res_abs_0_w[16 +:8] + res_abs_0_w[24 +:8] ;
-        sum_1_r <= sum_1_w + res_abs_1_w[0 +:8] + res_abs_1_w[8 +:8] + res_abs_1_w[16 +:8] + res_abs_1_w[24 +:8] ;
-        sum_2_r <= sum_2_w + res_abs_2_w[0 +:8] + res_abs_2_w[8 +:8] + res_abs_2_w[16 +:8] + res_abs_2_w[24 +:8] ;
-        sum_3_r <= sum_3_w + res_abs_3_w[0 +:8] + res_abs_3_w[8 +:8] + res_abs_3_w[16 +:8] + res_abs_3_w[24 +:8] ;
-        sum_4_r <= sum_4_w + res_abs_4_w[0 +:8] + res_abs_4_w[8 +:8] + res_abs_4_w[16 +:8] + res_abs_4_w[24 +:8] ;
+      if( flg_busy_opt_d3_r ) begin
+        sum_0_r <= sum_0_w + sum_abs_0_r ;
+        sum_1_r <= sum_1_w + sum_abs_1_r ;
+        sum_2_r <= sum_2_w + sum_abs_2_r ;
+        sum_3_r <= sum_3_w + sum_abs_3_r ;
+        sum_4_r <= sum_4_w + sum_abs_4_r ;
       end
     end
   end
 
+assign sum_abs_0_w  = res_abs_0_w[0 +:8] + res_abs_0_w[8 +:8] + res_abs_0_w[16 +:8] + res_abs_0_w[24 +:8];
+assign sum_abs_1_w  = res_abs_1_w[0 +:8] + res_abs_1_w[8 +:8] + res_abs_1_w[16 +:8] + res_abs_1_w[24 +:8];
+assign sum_abs_2_w  = res_abs_2_w[0 +:8] + res_abs_2_w[8 +:8] + res_abs_2_w[16 +:8] + res_abs_2_w[24 +:8];
+assign sum_abs_3_w  = res_abs_3_w[0 +:8] + res_abs_3_w[8 +:8] + res_abs_3_w[16 +:8] + res_abs_3_w[24 +:8];
+assign sum_abs_4_w  = res_abs_4_w[0 +:8] + res_abs_4_w[8 +:8] + res_abs_4_w[16 +:8] + res_abs_4_w[24 +:8];
 
 // ---   FSM: CMP (Compare)   -----------------------------
   // bst_type_r
